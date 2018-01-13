06cdcf96d3cd2dfe9fb39ea1211e3e3548c81b40
vthriller <unixway.drive+git@gmail.com>
2018-01-13 20:29:45 +0300

    move string literal parser into separate mod

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #[macro_use]
 extern crate nom;
 
+pub(crate) mod str;
 pub mod vec;
 pub mod expr;
--- src/vec.rs
+++ src/vec.rs
@@ -1,4 +1,5 @@
 use nom::{alpha, alphanumeric, digit};
+use str::string;
 
 /// Label filter operators.
 #[derive(Debug, PartialEq)]
@@ -150,59 +151,6 @@ named!(label_op <LabelMatchOp>, alt!(
 	| tag!("!~") => { |_| LabelMatchOp::RNe }
 	| tag!("=")  => { |_| LabelMatchOp::Eq  } // should come after =~
 	| tag!("!=") => { |_| LabelMatchOp::Ne  }
-));
-
-// > Label values may contain any Unicode characters.
-// > PromQL follows the same [escaping rules as Go](https://golang.org/ref/spec#String_literals).
-
-/* TODO
-\OOO (oct)
-\xXX
-\uXXXX (std::char::from_u32)
-\UXXXXXXXX (std::char::from_u32)
-
-TODO? should we really care whether \' is used in ""-strings or vice versa? (Prometheus itself does…)
-*/
-named!(rune <char>, preceded!(char!('\\'),
-        alt!(
-		map!(one_of!("abfnrtv\\'\""), |c| match c {
-			'a' => '\u{0007}',
-			'b' => '\u{0008}',
-			'f' => '\u{000c}',
-			'n' => '\n',
-			'r' => '\r',
-			't' => '\t',
-			'v' => '\u{000b}',
-			'\\' => '\\',
-			'\'' => '\'',
-			'"' => '"',
-			_ => unreachable!(),
-		})
-	)
-));
-
-named!(string <String>, alt!(
-	do_parse!(
-		char!('"') >>
-		s: many0!(alt!(rune | none_of!("\"\\"))) >>
-		char!('"') >>
-		(s.into_iter().collect())
-	)
-	|
-	do_parse!(
-		char!('\'') >>
-		s: many0!(alt!(rune | none_of!("'\\"))) >>
-		char!('\'') >>
-		(s.into_iter().collect())
-	)
-	|
-	do_parse!(
-		// raw string literals, where "backslashes have no special meaning"
-		char!('`') >>
-		s: map_res!(is_not!("`"), |s: &[u8]| String::from_utf8(s.to_vec())) >>
-		char!('`') >>
-		(s)
-	)
 ));
 
 #[cfg(test)]
