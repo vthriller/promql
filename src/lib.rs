8e33bf2b106af8e37fc29aa4d0ff124ad4797d96
vthriller <unixway.drive+git@gmail.com>
2018-01-25 14:39:45 +0300

    provide user with the actual AST, without trailing empty string

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,6 @@ pub(crate) mod expr;
 pub use vec::*;
 pub use expr::*;
 
-use nom::IResult;
 use nom::types::CompleteByteSlice;
 
 /**
@@ -18,6 +17,10 @@ Parse expression string into an AST.
 
 This parser operates on byte sequence instead of `&str` because of the fact that PromQL, like Go, allows raw byte sequences to be included in the string literals (e.g. `{omg='∞'}` is equivalent to both `{omg='\u221e'}` and `{omg='\xe2\x88\x9e'}`).
 */
-pub fn parse(e: &[u8]) -> IResult<CompleteByteSlice, expr::Node> {
-	expr::expression(CompleteByteSlice(e))
+pub fn parse(e: &[u8]) -> Result<Node, nom::Err<CompleteByteSlice>> {
+	match expression(CompleteByteSlice(e)) {
+		Ok((CompleteByteSlice(b""), ast)) => Ok(ast),
+		Ok(_) => unimplemented!("random gibberish at the end"),
+		Err(e) => Err(e),
+	}
 }
